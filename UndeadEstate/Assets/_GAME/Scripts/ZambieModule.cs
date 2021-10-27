6fab06bee28fe4d104397669f27468c4d72440b9
MrSuicideFish <malcolm259@gmail.com>
2021-10-27 01:26:01 -0700

    basic zambie behaviour

--- UndeadEstate/Assets/_GAME/Scripts/ZambieModule.cs
+++ UndeadEstate/Assets/_GAME/Scripts/ZambieModule.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Mirror;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZambieModule : MonoBehaviour
 {
+    private GameObject target;
+
+    private NavMeshAgent _navMeshAgent;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (target == null)
+        {
+            target = NetworkServer.spawned[NetworkServer.localConnection.identity.netId].gameObject;
+            return;
+        }
         
+        _navMeshAgent.SetDestination(target.transform.position);
     }
 }

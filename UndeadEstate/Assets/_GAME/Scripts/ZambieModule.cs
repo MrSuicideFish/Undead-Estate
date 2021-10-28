04304ee990eaa71eaee4ba8693d161395856d98c
MrSuicideFish <malcolm259@gmail.com>
2021-10-28 12:49:49 -0700

    updated player char layer

--- UndeadEstate/Assets/_GAME/Scripts/ZambieModule.cs
+++ UndeadEstate/Assets/_GAME/Scripts/ZambieModule.cs
@@ -1,34 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Mirror;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class ZambieModule : MonoBehaviour
 {
-    private GameObject target;
-
-    private NavMeshAgent _navMeshAgent;
+    private const float SEARCH_DELAY_BUFFER = 0.4f;
     
-    // Start is called before the first frame update
+    public float targetSearchDelay = 1.0f; 
+    private GameObject target;
+    private NavMeshAgent _navMeshAgent;
+
+    private float targetSearchTime = 0.0f;
+    private float searchTime = 0.0f;
+    
     void Start()
     {
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
+        targetSearchTime = Random.Range(targetSearchDelay - SEARCH_DELAY_BUFFER, 
+            targetSearchDelay + SEARCH_DELAY_BUFFER);
+        
+        // set start target
+        target = FindNearestTarget(this.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (NetworkClient.isConnected && NetworkClient.Ready() && NetworkClient.isHostClient)
+        // search for new target
+        searchTime += Time.deltaTime;
+        if (searchTime >= targetSearchTime)
         {
-            if (target == null)
-            {
-                target = NetworkServer.spawned[NetworkServer.localConnection.identity.netId].gameObject;
-                return;
-            }
-            
+            target = FindNearestTarget(this.transform);
+            searchTime = 0.0f;
+        }
+
+        // move towards target or stop
+        if (target != null)
+        {
             _navMeshAgent.SetDestination(target.transform.position);
         }
+    }
+
+    public void SetTarget(GameObject newTarget)
+    {
+        target = newTarget;
+    }
+
+    protected static GameObject FindNearestTarget(Transform zambie)
+    {
+        Collider[] hitColliders =
+            Physics.OverlapSphere(zambie.position, 500, LayerMask.GetMask("Survivors"));
+
+        if (hitColliders == null || hitColliders.Length == 0) return null;
+
+        foreach (Collider col in hitColliders)
+            if (col.gameObject != null) return col.gameObject;
+        return null;
     }
 }
